97a0ddddf5ec3eefef0e7b99fa35623c381b5183
Gorbataras <gorbataras@gmail.com>
2020-01-27 00:10:57 -0500

    Completed personal info page and profile page

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-
+session_start();
 // Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -15,6 +15,36 @@ $f3 = Base::instance();
 $f3->route('GET /', function () {
     $view = new Template();
     echo $view->render('views/home.html');
+});
+
+//TO PERSONAL INFO PAGE
+$f3->route('GET /personal', function () {
+    $view = new Template();
+    echo $view->render('views/personal_info.html');
+});
+
+//TO PROFILE PAGE
+$f3->route('POST /profile', function () {
+    $_SESSION['fname'] = $_POST['fname'];
+    $_SESSION['lname'] = $_POST['lname'];
+    $_SESSION['age'] = $_POST['age'];
+    $_SESSION['gender'] = $_POST['gender'];
+    $_SESSION['phone'] = $_POST['phone'];
+
+    $view = new Template();
+    echo $view->render('views/profile.html');
+});
+
+//TO INTERESTS PAGE
+$f3->route('GET /interests', function () {
+    $view = new Template();
+    echo $view->render('views/interests.html');
+});
+
+//TO SUMMARY PAGE
+$f3->route('GET /summary', function () {
+    $view = new Template();
+    echo $view->render('views/summary.html');
 });
 
 // Run F3
